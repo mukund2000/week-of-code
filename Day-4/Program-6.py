6957b2426c1f572be88ad805650aac1bc1adb338
mukund2000 <mukundrastogixyz@gmail.com>
2020-05-22 16:16:32 +0530

    Special problem

--- Day-4/Program-6.py
+++ Day-4/Program-6.py
@@ -5,3 +5,18 @@ Created on Fri May 22 15:55:32 2020
 @author: Mukund Rastogi
 """
 
+sd=int(input("Enter the size of dictionary"))
+dictionary=[]
+for i in range(sd):
+    dictionary.append(input("Enter words in list: "))
+
+ar=[]
+res=[]
+sa=int(input("Enter the size of array"))
+for i in range(sa):
+    ar.append(input("Enter characters in array: "))
+for word in dictionary:
+    if set(word).issubset(set(ar)):
+        res.append(word)
+
+print(",".join(res)+".")
